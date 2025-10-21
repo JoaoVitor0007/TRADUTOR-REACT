7480df8c28a866cedaa8b97b77355cbef889649c
Joao Vitor <tkirito940@gmail.com>
2025-10-21 12:12:34 -0400

    pagina inicial

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ActionCards from './componentes/action-cards';
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}>CONVERTER TEXTOS EM FALA</Text>
+      <Text style={styles.subtitulo}>Nossa tecnologia transforma texto em fala com vozes realistas e suporte a vários idiomas — ideal para pessoas com baixa visão ou com dificuldades de leitura, além de útil para criar narrações, áudios e facilitar a acessibilidade.</Text>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 50, gap: 80 }}>
+        <ActionCards img={require('./assets/audio.png')} titulo="Leitura em voz alta" texto="Ouça qualquer texto de forma clara e natural." />
+        <ActionCards img={require('./assets/multimaior.png')} titulo="Suporte Multilíngue" texto="Converta textos em vários idiomas." />
+        <ActionCards img={require('./assets/microfone.png')} titulo="Vozes Naturais" texto="Áudios realistas que soam humanos" />
+        <ActionCards img={require('./assets/deficiente.png')} titulo="Acessibilidade
+Digital" texto="Inclusão para pessoas com deficiência visual" />
+
+
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Home: HomeScreen,
+    //sobre:
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
+
+export default function App() {
+  return <Navigation />;
+}
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#053B68',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 40,
+    gap: 20,
+    paddingTop: 80,
+    
+  },
+  titulo: {
+    fontSize: 96,
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+  },
+  subtitulo: {
+    fontSize: 30,
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    paddingTop: 10,
+    textAlign: 'center',
   },
 });
---
+++ componentes/action-cards.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function ActionCards( {img, titulo, texto} = {}) {
+    return (
+        <View style={styles.container}>
+            <Image style={styles.tinyLogo} source={img} />
+            <Text style={styles.titulo}>{titulo}</Text>
+            <Text style={styles.subtitulo}>{texto}</Text>
+        </View >
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: 272,
+        backgroundColor: '#06467aff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        shadowColor: '#1B0046',
+        shadowOffset: { width: 0, height: 0 },
+        shadowOpacity: 0.95,
+        shadowRadius: 85,
+        fontWeight: 'bold',
+        padding: 20,
+        borderRadius: 20,
+    },
+    titulo: {
+        fontSize: 25,
+        color: '#ffffffff',
+        fontWeight: 'bold',
+        paddingTop: 15,
+        textAlign: 'center',
+    },
+    subtitulo: {
+        fontSize: 20,
+        color: '#FFFFFF',
+        fontWeight: 'bold',
+        paddingTop: 10,
+        textAlign: 'center',
+    },
+    tinyLogo: {
+    width: 120,
+    height: 120,
+  },
+});
